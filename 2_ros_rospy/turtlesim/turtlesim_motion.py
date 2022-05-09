51cc6a1ed12d4589adb7b967f225d1c70191d631
Patrik <golecpatrik@gmail.com>
2022-05-09 18:57:48 +0200

    fix turtlesim script

--- 2_ros_rospy/turtlesim/turtlesim_motion.py
+++ 2_ros_rospy/turtlesim/turtlesim_motion.py
@@ -25,7 +25,7 @@ def poseCallback(pose_message):
     # rospy.loginfo(yaw)
 
 
-def distance_2d(p1, p2, q1, q2):
+def distance_2d(p1, p2):
     """
         Euklidischer Abstand: Abstand zweier Punkte in der Ebene
         Formel: math.sqrt( (q1 - p1)**2 + ... + (qn - pn)**2 )
@@ -34,14 +34,15 @@ def distance_2d(p1, p2, q1, q2):
 
         Abstand von x0,y0 zu momentanem x,y
     """
+    global x, y
 
-    return math.sqrt( (q1 - p1)**2 + (p2- q2)**2 )
+    return  abs( math.sqrt( (x - p1)**2 + (y - p2)**2 ) )
 
 
 def move(speed, distance, is_forward=True):
     velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
-    rate = rospy.Rate(1) #hz
+    rate = rospy.Rate(10) #hz
 
     velocity_msg = Twist()
 
@@ -57,15 +58,15 @@ def move(speed, distance, is_forward=True):
     distance_moved = 0.0
 
     while not rospy.is_shutdown():
-        if distance_moved + velocity_msg.linear.x <= distance:
-            distance_moved += distance_2d(x, y, x0, y0)
+        if distance_moved + abs(speed) <= distance:
             velocity_publisher.publish(velocity_msg)
+            distance_moved = distance_2d(x0, y0)
         else:
             velocity_msg.linear.x = 0
             velocity_publisher.publish(velocity_msg)
-
-        #rospy.loginfo(velocity_msg.linear.x)
-        #rospy.loginfo(distance_moved)
+            
+        # rospy.loginfo(x)
+        rospy.loginfo("distance moved: " + str(distance_moved))
 
         rate.sleep()
 
@@ -73,27 +74,36 @@ def move(speed, distance, is_forward=True):
 def go_to(goal_x, goal_y):
     velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 
-    rate = rospy.Rate(55) #hz
+    rate = rospy.Rate(10) #hz
 
     velocity_msg = Twist()
 
     global x, y, yaw
 
     while not rospy.is_shutdown():
-        distance = distance_2d(goal_x, goal_y, x, y)
+        distance = distance_2d(goal_x, goal_y)
 
-        LINEAR_CONSTANT_SPEED = 1.0
+        LINEAR_CONSTANT_SPEED = 0.5
         linear_speed = distance * LINEAR_CONSTANT_SPEED
         
-        ANGULAR_CONSTANT_SPEED = 5.0
+        ANGULAR_CONSTANT_SPEED = 3.0
         desired_angle = math.atan2(goal_y - y, goal_x - x)
         angular_speed = (desired_angle - yaw) * ANGULAR_CONSTANT_SPEED
-
+	
         if distance > 0.01:
             velocity_msg.linear.x = linear_speed
             velocity_msg.angular.z = angular_speed
             velocity_publisher.publish(velocity_msg)
+        else:
+            velocity_msg.linear.x = 0
+            velocity_msg.angular.z = 0
+            velocity_publisher.publish(velocity_msg)
+        
 
+        # rospy.loginfo(linear_speed)
+        # rospy.loginfo(desired_angle)
+        # rospy.loginfo(distance)
+        
         rate.sleep()
 
 
@@ -101,8 +111,11 @@ if __name__ == '__main__':
     try:
         rospy.init_node('my_turtlesim_node', anonymous=True)
         subscriber = rospy.Subscriber('/turtle1/pose', Pose, poseCallback)
-
-        move(2.0, 10.0, True)
-        # go_to(10.0, 10.0)
+        
+        # sleep, sonst sind x, y initial 0
+        rospy.sleep(2.0)
+        
+        move(1.0, 4.0, True)
+        # go_to(1.0, 7)
     except rospy.ROSInterruptException:
         pass
